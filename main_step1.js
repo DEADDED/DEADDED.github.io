07179c6729c2939cf5d63943d7d32cd11a943769
Alexey <mr.gomel113@yandex.ru>
2018-03-18 13:04:51 +0300

    Update main_step1.js

--- main_step1.js
+++ main_step1.js
@@ -239,7 +239,7 @@ function loop(){
 
 function updatePlane(){
   var targetY = 5; //mousePos1.y;//normalize(mousePos1.y,-.75,.75,25, 175);
-  var targetX = mousePos1.x; //normalize(mousePos1.x,-.75,.75,-100, 100);
+  var targetX = normalize(mousePos1.x,-.75,.75,-100, 100);
   airplane.mesh.position.y = targetY;
   airplane.mesh.position.x = targetX;
   airplane.propeller.rotation.x += 0.3;
@@ -268,7 +268,7 @@ function init(event){
 var mousePos1 = { x: 0, y: 0 };
 
 function handleOrientation(event){
-	var tx = event.beta;
+	var tx = event.alpha;
 	//alert(tx);
 	console.log(tx);
 	mousePos1={x:tx, y:15};
