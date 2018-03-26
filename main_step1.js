a9537d8436536f8f7878097eb311b642cd91c98e
Alexey <mr.gomel113@yandex.ru>
2018-03-26 23:05:58 +0300

    Update main_step1.js

--- main_step1.js
+++ main_step1.js
@@ -1,23 +1,6 @@
 //COLORS
 
-var start = function() {
-    screen.orientation.lock('landscape-primary').then(
-      startInternal,
-      function() {
-        alert('To start, rotate your screen to landscape.');
 
-        var orientationChangeHandler = function() {
-          if (!screen.orientation.type.startsWith('landscape')) {
-            return;
-          }
-          screen.orientation.removeEventListener('change', orientationChangeHandler);
-          startInternal();
-        }
-
-        screen.orientation.addEventListener('change', orientationChangeHandler);
-      });
-  }
-  window.onload = start;
 
 var Colors = {
     red:0xf25346,
