a25a293e6a2d11db1c78e137c3d881565a2b80b5
Alexey <mr.gomel113@yandex.ru>
2018-04-16 14:22:24 +0300

    Add files via upload

--- space/js/game.js
+++ space/js/game.js
@@ -22,7 +22,7 @@ function createScene() {
     nearPlane,
     farPlane
     );
-  scene.fog = new THREE.Fog(0xf7d9aa, 100,950);
+  scene.fog = new THREE.Fog(0xff0000, 100,950);
   camera.position.x = 0;
   camera.position.z = 200;
   camera.position.y = 100;
@@ -72,7 +72,107 @@ function createLights() {
 
 
 
+var AirPlane = function(){
+	this.mesh = new THREE.Object3D();
+  this.mesh.name = "airPlane";
+
+  // Create the cabin
+	var geomCockpit = new THREE.BoxGeometry(60,50,50,1,1,1);
+  var matCockpit = new THREE.MeshPhongMaterial({color:0x550000, shading:THREE.FlatShading});
+  var cockpit = new THREE.Mesh(geomCockpit, matCockpit);
+	cockpit.castShadow = true;
+  cockpit.receiveShadow = true;
+  this.mesh.add(cockpit);
+
+  // Create Engine
+  var geomEngine = new THREE.BoxGeometry(20,50,50,1,1,1);
+  var matEngine = new THREE.MeshPhongMaterial({color:0xffffff, shading:THREE.FlatShading});
+  var engine = new THREE.Mesh(geomEngine, matEngine);
+  engine.position.x = 40;
+  engine.castShadow = true;
+  engine.receiveShadow = true;
+	this.mesh.add(engine);
+
+  // Create Tailplane
+
+  var geomTailPlane = new THREE.BoxGeometry(15,20,5,1,1,1);
+  var matTailPlane = new THREE.MeshPhongMaterial({color:0x550000, shading:THREE.FlatShading});
+  var tailPlane = new THREE.Mesh(geomTailPlane, matTailPlane);
+  tailPlane.position.set(-35,25,0);
+  tailPlane.castShadow = true;
+  tailPlane.receiveShadow = true;
+	this.mesh.add(tailPlane);
+
+  // Create Wing
+
+  var geomSideWing = new THREE.BoxGeometry(40,8,150,1,1,1);
+  var matSideWing = new THREE.MeshPhongMaterial({color:0x550000, shading:THREE.FlatShading});
+  var sideWing = new THREE.Mesh(geomSideWing, matSideWing);
+  sideWing.position.set(0,0,0);
+  sideWing.castShadow = true;
+  sideWing.receiveShadow = true;
+	this.mesh.add(sideWing);
+
+  // Propeller
+
+  var geomPropeller = new THREE.BoxGeometry(20,10,10,1,1,1);
+  var matPropeller = new THREE.MeshPhongMaterial({color:0x555500, shading:THREE.FlatShading});
+  this.propeller = new THREE.Mesh(geomPropeller, matPropeller);
+  this.propeller.castShadow = true;
+  this.propeller.receiveShadow = true;
+
+  // Blades
+
+  var geomBlade = new THREE.BoxGeometry(1,100,20,1,1,1);
+  var matBlade = new THREE.MeshPhongMaterial({color:0x111100, shading:THREE.FlatShading});
+
+  var blade = new THREE.Mesh(geomBlade, matBlade);
+  blade.position.set(8,0,0);
+  blade.castShadow = true;
+  blade.receiveShadow = true;
+	this.propeller.add(blade);
+  this.propeller.position.set(50,0,0);
+  this.mesh.add(this.propeller);
+};
+
+var airplane;
+
+function createPlane(){
+  airplane = new AirPlane();
+  airplane.mesh.scale.set(.25,.25,.25);
+  airplane.mesh.position.y = 100;
+  scene.add(airplane.mesh);
+}
+
+function updatePlane(){
+  var targetY = 30; //mousePos1.y;//normalize(mousePos1.y,-.75,.75,25, 175);
+  var targetX = mousePos1.x;// normalize(mousePos1.x,-.75,.75,-100, 100);
+  /*if(targetX > 100)
+	  targetX = 100;
+  else if(targetX < -100)
+	  targetX = -100;*/
+  airplane.mesh.position.y = targetY;
+  airplane.mesh.position.x += targetX;
+	if(airplane.mesh.position.x > 100)
+		airplane.mesh.position.x = 100;
+	if(airplane.mesh.position.x < -100)
+		airplane.mesh.position.x = -100;
+	
+  airplane.propeller.rotation.x += 0.3;
+}
+
+var mousePos1 = { x: 0, y: 0 };
+function handleOrientation(event){
+	//tx = mousePos1.x;
+	var tx = event.accelerationIncludingGravity.y;
+	//alert(event.alpha);
+	//alert(tx);
+	//console.log(tx);
+	mousePos1={x:tx, y:45};
+}
 function loop(){
+  
+  updatePlane();
   renderer.render(scene, camera);
   requestAnimationFrame(loop);
 }
@@ -80,28 +180,11 @@ function loop(){
 
 function init(event){
   
+  window.addEventListener('devicemotion', handleOrientation);
+
   createScene();
   createLights();
-  
-
-  var loader = new THREE.JSONLoader();
-  loader.load( "https://github.com/DEADDED/DEADDED.github.io/tree/master/space/js/flamingo.js", function( geometry ) {
-      var material = new THREE.MeshPhongMaterial( {
-          color: 0xffffff,
-          morphTargets: true,
-          vertexColors: THREE.FaceColors,
-          flatShading: true
-      } );
-      var mesh = new THREE.Mesh( geometry, material );
-      mesh.position.x = - 150;
-      mesh.position.y = 150;
-      mesh.scale.set( 1.5, 1.5, 1.5 );
-      scene.add( mesh );
-      var mixer = new THREE.AnimationMixer( mesh );
-      mixer.clipAction( geometry.animations[ 0 ] ).setDuration( 1 ).play();
-      mixers.push( mixer );
-  } );
-
+  createPlane();
   loop();
 }
 
