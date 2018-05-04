2a8fb142dfc65372a2bf8f583a4a3d497a546c57
Alexey <mr.gomel113@yandex.ru>
2018-05-04 23:07:41 +0300

    Update game.js

--- runrun/js/game.js
+++ runrun/js/game.js
@@ -128,7 +128,7 @@ function init(event){
           flamingo = new THREE.Mesh(geometry, material);
 
           flamingo.position.x = 0;
-          flamingo.position.y = 100;
+          flamingo.position.y = 50;
           flamingo.scale.set(1.5,1.5,1.5);
 	  flamingo.rotateY(Math.PI);
 
