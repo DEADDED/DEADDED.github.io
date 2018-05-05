23950618685853dd9cc0c94b8ab141f0d7861dd9
Alexey <mr.gomel113@yandex.ru>
2018-05-05 15:10:23 +0300

    Update game.js

--- runrun/js/game.js
+++ runrun/js/game.js
@@ -79,7 +79,56 @@ function createLights() {
 
 
 
+Sea = function(){
+  var geom = new THREE.CylinderGeometry(600,600,800,40,10);
+  geom.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI/2));
+  geom.mergeVertices();
+  var l = geom.vertices.length;
 
+  this.waves = [];
+
+  for (var i=0;i<l;i++){
+    var v = geom.vertices[i];
+    this.waves.push({y:v.y,
+                     x:v.x,
+                     z:v.z,
+                     ang:Math.random()*Math.PI*2,
+                     amp:5 + Math.random()*15,
+                     speed:0.016 + Math.random()*0.032
+                    });
+  };
+  var mat = new THREE.MeshPhongMaterial({
+    color:Colors.blue,
+    transparent:true,
+    opacity:.8,
+    shading:THREE.FlatShading,
+
+  });
+
+  this.mesh = new THREE.Mesh(geom, mat);
+  this.mesh.receiveShadow = true;
+
+}
+
+Sea.prototype.moveWaves = function (){
+  var verts = this.mesh.geometry.vertices;
+  var l = verts.length;
+  for (var i=0; i<l; i++){
+    var v = verts[i];
+    var vprops = this.waves[i];
+    v.x =  vprops.x + Math.cos(vprops.ang)*vprops.amp;
+    v.y = vprops.y + Math.sin(vprops.ang)*vprops.amp;
+    vprops.ang += vprops.speed;
+  }
+  this.mesh.geometry.verticesNeedUpdate=true;
+  sea.mesh.rotation.z += .005;
+}
+
+function createSea(){
+  sea = new Sea();
+  sea.mesh.position.y = -600;
+  scene.add(sea.mesh);
+}
 
 
 
@@ -89,7 +138,7 @@ function loop(){
 	
   requestAnimationFrame(loop);
   //camera.rotation.z += 0.001;
-
+sea.moveWaves();
   var delta = clock.getDelta();
   mixer.update(delta);
 				/*for ( var i = 0; i < mixers.length; i ++ ) {
@@ -111,6 +160,7 @@ function init(event){
 
   createScene();
   createLights();
+  createSea();
 
   clock = new THREE.Clock();
 
