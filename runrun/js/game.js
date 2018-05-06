f4cfc16e03e38bda6db5819bcadca5a6957e605d
Alexey <mr.gomel113@yandex.ru>
2018-05-06 23:46:00 +0300

    Update game.js

--- runrun/js/game.js
+++ runrun/js/game.js
@@ -210,7 +210,7 @@ function init(event){
   var flaPos = new THREE.Vector3();
   flaPos = flaPos.setFromMatrixPosition( flamingo.matrixWorld );
 	//treePos.distanceTo(heroSphere.position)
-  alert(flaPos.distanceTo(Ground.position);
+  alert(flaPos.distanceTo(Ground.position));
   loop();
 }
 var fPos, oldPos;
