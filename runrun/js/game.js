c2301db920155074407762a68ae980bab9597eae
Alexey <mr.gomel113@yandex.ru>
2018-05-08 22:18:18 +0300

    Update game.js

--- runrun/js/game.js
+++ runrun/js/game.js
@@ -243,7 +243,7 @@ function updateFlamingo(){
   flaPos = flaPos.setFromMatrixPosition( flamingo.matrixWorld );
 	//alert(flaPos);
 	//treePos.distanceTo(heroSphere.position)
-  console.log(flaPos.distanceTo(ground.mesh.position));
+  console.log(flaPos.distanceTo(obl.position));
 	/*if(oldPos < fPos)
 		flamingo.rotation.z += 0.03;
 	else if (oldPos> fPos)
