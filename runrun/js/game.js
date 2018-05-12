ec9e0b330d4d261f80d9017ee633d8907fc37c54
Alexey <mr.gomel113@yandex.ru>
2018-05-12 16:35:30 +0300

    Update game.js

--- runrun/js/game.js
+++ runrun/js/game.js
@@ -326,8 +326,8 @@ function loop(){
 		cloudRows[i].mesh.getWorldPosition(targetVector);
 		cloudRows[i].mesh.rotation.x += 0.009;
 		
-		cloudRows[i].mesh.position.z += cloudH * Math.cos(cloudRows[i].rot + rotUpdate);
-		cloudRows[i].mesh.position.y += -cloudH * Math.cos(cloudRows[i].rot + rotUpdate);
+		cloudRows[i].mesh.position.z = cloudH * Math.cos(cloudRows[i].rot + rotUpdate);
+		cloudRows[i].mesh.position.y = -cloudH * Math.cos(cloudRows[i].rot + rotUpdate);
 		
 		console.log(cloudRows[i].mesh.position.z,"  ", cloudRows[i].mesh.position.y);
 		/*if(targetVector.z > 500 &&  targetVector.y > 0)
