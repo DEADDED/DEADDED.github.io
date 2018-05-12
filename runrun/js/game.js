17a648f52a08d4a4e3abd66a86229237af684adf
Alexey <mr.gomel113@yandex.ru>
2018-05-12 17:47:00 +0300

    Update game.js

--- runrun/js/game.js
+++ runrun/js/game.js
@@ -383,7 +383,7 @@ function init(event){
 	  
 	  cloudRows[i].addClouds();
 	  
-	  cloudRows[i].mesh.position.z = -cloudH * Math.cos(Math.PI/2 + Math.PI/12 * i);
+	  cloudRows[i].mesh.position.z = cloudH * Math.cos(Math.PI/2 + Math.PI/12 * i);
 	  cloudRows[i].mesh.position.y = cloudH * Math.sin(Math.PI/2 + Math.PI/12 * i);
 	  cloudRows[i].rot = globalRot * i;
 	  
