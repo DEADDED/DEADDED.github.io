d2aa9ec4cad72d29f5cb456d515db8f634c26428
Alexey <mr.gomel113@yandex.ru>
2018-05-12 13:11:57 +0300

    Update game.js

--- runrun/js/game.js
+++ runrun/js/game.js
@@ -316,7 +316,7 @@ function loop(){
 	
 	for(var i = 0; i < 4; i++){
 		cloudRows[i].mesh.getWorldPosition(targetVector);
-		if(targetVector.z > 600)
+		if(targetVector.z > 600 && targetVector.z < 605)
 		   console.log("gone");
 		
 		/*if(targetVector.z >  cloudH / 2 + 200)
