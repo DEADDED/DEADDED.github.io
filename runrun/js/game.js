7724181eb0eca9fcb03f3fd48c60c5f8cd8534b7
Alexey <mr.gomel113@yandex.ru>
2018-05-13 23:46:09 +0300

    Update game.js

--- runrun/js/game.js
+++ runrun/js/game.js
@@ -180,7 +180,7 @@ function createGround(){
 Cloud = function(){
   this.mesh = new THREE.Object3D();
  //var geom = new THREE.BoxGeometry(15,15,15);
-	var geom = new THREE.SphereGeometry(15,15,15);
+	var geom = new THREE.SphereGeometry(15,8,8);
 	
  var mat = new THREE.MeshPhongMaterial({
  	color: 0xffffff});
