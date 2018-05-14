7651233a15739210187e9ab24c5b901465d13184
Alexey <mr.gomel113@yandex.ru>
2018-05-14 19:03:57 +0300

    Update game.js

--- flamingo-flight/js/game.js
+++ flamingo-flight/js/game.js
@@ -130,7 +130,7 @@ function createScene() {
   camera.position.y = 950;
   camera.position.z = 600;
   
-  renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+  renderer = new THREE.WebGLRenderer();
   renderer.setSize(WIDTH, HEIGHT);
   renderer.shadowMap.enabled = true;
   container = document.getElementById('world');
