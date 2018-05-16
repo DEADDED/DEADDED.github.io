999e40c84519add854fb1229985d95b1382df061
Alexey <mr.gomel113@yandex.ru>
2018-05-16 22:19:55 +0300

    Update game.js

--- flamingo-flight/js/game.js
+++ flamingo-flight/js/game.js
@@ -374,7 +374,7 @@ cloudRow.prototype.addClouds = function(){
 
 //Function for updating each object in a scene
 function loop(){
-WiDTH = window.innerWidth;
+WIDTH = window.innerWidth;
 HEIGHT = window.innerHeight;
 	
   requestAnimationFrame(loop);                                      //update an animation before the next repaint
