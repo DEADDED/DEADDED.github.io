9108865d7dc0ab7321a36bcc090593a9dc677e5d
Alexey <mr.gomel113@yandex.ru>
2018-05-17 00:20:57 +0300

    Update game.js

--- flamingo-flight/js/game.js
+++ flamingo-flight/js/game.js
@@ -82,11 +82,11 @@ function init(event){
           
   flamingo = new THREE.Mesh(geometry, material);
 	  
-  flamingo.position.x = -85;
+  flamingo.position.x = 0;
   flamingo.position.y = 930;
 	flamingo.position.z = 480;
   flamingo.scale.set(.3,.3,.3);
-	flamingo.rotateY(Math.PI/4);
+	flamingo.rotateY(Math.PI/6);
 
   scene.add(flamingo);
     
