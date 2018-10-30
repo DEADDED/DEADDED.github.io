b485d45f5037dbf8061e7f6d10d801c2589f3c15
Alexey <mr.gomel113@yandex.ru>
2018-10-31 00:25:28 +0300

    Update gameN.js

--- flamingo-flight-2/js/gameN.js
+++ flamingo-flight-2/js/gameN.js
@@ -326,6 +326,6 @@ window.addEventListener('load', init, false);
 }
 else{
    alert("not mobile"); 
-  var elem = document.getElementById("world);
+  var elem = document.getElementById("world");
   elem.style.opacity = 0;
 }
